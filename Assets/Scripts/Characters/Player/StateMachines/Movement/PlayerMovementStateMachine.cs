aaebb8f243fbe02e724960e41f8a6f663ede3942
Wafflus <wafflusdev@gmail.com>
2022-04-02 22:29:17 +0100

    Update player movement state machine to cache player states

--- Assets/Scripts/Characters/Player/StateMachines/Movement/PlayerMovementStateMachine.cs
+++ Assets/Scripts/Characters/Player/StateMachines/Movement/PlayerMovementStateMachine.cs
@@ -2,5 +2,19 @@ namespace GenshinImpactMovementSystem
 {
     public class PlayerMovementStateMachine : StateMachine
     {
+        public PlayerIdlingState IdlingState { get; }
+
+        public PlayerWalkingState WalkingState { get; }
+        public PlayerRunningState RunningState { get; }
+        public PlayerSprintingState SprintingState { get; }
+
+        public PlayerMovementStateMachine()
+        {
+            IdlingState = new PlayerIdlingState();
+
+            WalkingState = new PlayerWalkingState();
+            RunningState = new PlayerRunningState();
+            SprintingState = new PlayerSprintingState();
+        }
     }
 }
