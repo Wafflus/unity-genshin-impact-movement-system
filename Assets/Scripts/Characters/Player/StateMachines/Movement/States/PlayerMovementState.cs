ad47597988aa7d4891235752695c1d530df76121
Wafflus <wafflusdev@gmail.com>
2022-04-02 22:14:29 +0100

    Add base player movement state script

---
+++ Assets/Scripts/Characters/Player/StateMachines/Movement/States/PlayerMovementState.cs
@@ -0,0 +1,25 @@
+namespace GenshinImpactMovementSystem
+{
+    public class PlayerMovementState : IState
+    {
+        public virtual void Enter()
+        {
+        }
+
+        public virtual void Exit()
+        {
+        }
+
+        public virtual void HandleInput()
+        {
+        }
+
+        public virtual void Update()
+        {
+        }
+
+        public virtual void PhysicsUpdate()
+        {
+        }
+    }
+}
