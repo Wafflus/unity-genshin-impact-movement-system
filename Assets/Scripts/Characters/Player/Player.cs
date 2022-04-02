90e3bfd5aca9d58a76c592233b98d3fd7810d0b6
Wafflus <wafflusdev@gmail.com>
2022-04-02 22:37:22 +0100

    Add player movement state machine to the player script

--- Assets/Scripts/Characters/Player/Player.cs
+++ Assets/Scripts/Characters/Player/Player.cs
@@ -4,5 +4,28 @@ namespace GenshinImpactMovementSystem
 {
     public class Player : MonoBehaviour
     {
+        private PlayerMovementStateMachine movementStateMachine;
+
+        private void Awake()
+        {
+            movementStateMachine = new PlayerMovementStateMachine();
+        }
+
+        private void Start()
+        {
+            movementStateMachine.ChangeState(movementStateMachine.IdlingState);
+        }
+
+        private void Update()
+        {
+            movementStateMachine.HandleInput();
+
+            movementStateMachine.Update();
+        }
+
+        private void FixedUpdate()
+        {
+            movementStateMachine.PhysicsUpdate();
+        }
     }
 }
