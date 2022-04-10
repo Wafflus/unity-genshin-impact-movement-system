9e2db38004783ed06c48ced178bfac227131b0c9
Wafflus <wafflusdev@gmail.com>
2022-04-10 16:47:52 +0100

    Add base player movement

--- Assets/Scripts/Characters/Player/StateMachines/Movement/States/PlayerMovementState.cs
+++ Assets/Scripts/Characters/Player/StateMachines/Movement/States/PlayerMovementState.cs
@@ -8,6 +8,9 @@ namespace GenshinImpactMovementSystem
 
         protected Vector2 movementInput;
 
+        protected float baseMovementSpeed = 5f;
+        protected float speedModifier = 1f;
+
         public PlayerMovementState(PlayerMovementStateMachine playerMovementStateMachine)
         {
             stateMachine = playerMovementStateMachine;
@@ -32,11 +35,47 @@ namespace GenshinImpactMovementSystem
 
         public virtual void PhysicsUpdate()
         {
+            Move();
         }
 
         private void ReadMovementInput()
         {
             movementInput = stateMachine.Player.Input.PlayerActions.Movement.ReadValue<Vector2>();
+        }
+
+        private void Move()
+        {
+            if (movementInput == Vector2.zero || speedModifier == 0f)
+            {
+                return;
+            }
+
+            Vector3 movementDirection = GetMovementInputDirection();
+
+            float movementSpeed = GetMovementSpeed();
+
+            Vector3 currentPlayerHorizontalVelocity = GetPlayerHorizontalVelocity();
+
+            stateMachine.Player.Rigidbody.AddForce(movementDirection * movementSpeed - currentPlayerHorizontalVelocity, ForceMode.VelocityChange);
+        }
+
+        protected Vector3 GetMovementInputDirection()
+        {
+            return new Vector3(movementInput.x, 0f, movementInput.y);
+        }
+
+        protected float GetMovementSpeed()
+        {
+            return baseMovementSpeed * speedModifier;
+        }
+
+        protected Vector3 GetPlayerHorizontalVelocity()
+        {
+            Vector3 playerHorizontalVelocity = stateMachine.Player.Rigidbody.velocity;
+
+            playerHorizontalVelocity.y = 0f;
+
+            return playerHorizontalVelocity;
         }
     }
 }
