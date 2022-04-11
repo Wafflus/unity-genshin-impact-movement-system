f2bf6984d4ce91634c0118d260254dd39b2cdf17
Wafflus <wafflusdev@gmail.com>
2022-04-11 20:18:25 +0100

    Add player dash data reference to the player grounded data script

--- Assets/Scripts/Characters/Player/Data/States/Grounded/PlayerGroundedData.cs
+++ Assets/Scripts/Characters/Player/Data/States/Grounded/PlayerGroundedData.cs
@@ -9,6 +9,7 @@ namespace GenshinImpactMovementSystem
         [field: SerializeField] [field: Range(0f, 25f)] public float BaseSpeed { get; private set; } = 5f;
         [field: SerializeField] public AnimationCurve SlopeSpeedAngles { get; private set; }
         [field: SerializeField] public PlayerRotationData BaseRotationData { get; private set; }
+        [field: SerializeField] public PlayerDashData DashData { get; private set; }
         [field: SerializeField] public PlayerWalkData WalkData { get; private set; }
         [field: SerializeField] public PlayerRunData RunData { get; private set; }
     }
