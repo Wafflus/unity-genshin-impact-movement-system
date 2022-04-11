0f157967c8dc8462c81eb282af609fcfed8c7d57
Wafflus <wafflusdev@gmail.com>
2022-04-11 17:28:56 +0100

    Add player scriptable object reference to the player script

--- Assets/Scripts/Characters/Player/Player.cs
+++ Assets/Scripts/Characters/Player/Player.cs
@@ -5,6 +5,9 @@ namespace GenshinImpactMovementSystem
     [RequireComponent(typeof(PlayerInput))]
     public class Player : MonoBehaviour
     {
+        [field: Header("References")]
+        [field: SerializeField] public PlayerSO Data { get; private set; }
+
         public Rigidbody Rigidbody { get; private set; }
 
         public PlayerInput Input { get; private set; }
