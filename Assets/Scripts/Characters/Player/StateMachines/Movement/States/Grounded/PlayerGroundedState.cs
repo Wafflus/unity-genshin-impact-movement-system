2bd91811955b06b5cb14d477974a7d48b6709554
Wafflus <wafflusdev@gmail.com>
2022-04-11 19:11:26 +0100

    Add float method to the player grounded state script

--- Assets/Scripts/Characters/Player/StateMachines/Movement/States/Grounded/PlayerGroundedState.cs
+++ Assets/Scripts/Characters/Player/StateMachines/Movement/States/Grounded/PlayerGroundedState.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace GenshinImpactMovementSystem
@@ -6,6 +7,36 @@ namespace GenshinImpactMovementSystem
     {
         public PlayerGroundedState(PlayerMovementStateMachine playerMovementStateMachine) : base(playerMovementStateMachine)
         {
+        }
+
+        public override void PhysicsUpdate()
+        {
+            base.PhysicsUpdate();
+
+            Float();
+        }
+
+        private void Float()
+        {
+            Vector3 capsuleColliderCenterInWorldSpace = stateMachine.Player.ResizableCapsuleCollider.CapsuleColliderData.Collider.bounds.center;
+
+            Ray downwardsRayFromCapsuleCenter = new Ray(capsuleColliderCenterInWorldSpace, Vector3.down);
+
+            if (Physics.Raycast(downwardsRayFromCapsuleCenter, out RaycastHit hit, stateMachine.Player.ResizableCapsuleCollider.SlopeData.FloatRayDistance, stateMachine.Player.LayerData.GroundLayer, QueryTriggerInteraction.Ignore))
+            {
+                float distanceToFloatingPoint = stateMachine.Player.ResizableCapsuleCollider.CapsuleColliderData.ColliderCenterInLocalSpace.y * stateMachine.Player.transform.localScale.y - hit.distance;
+
+                if (distanceToFloatingPoint == 0f)
+                {
+                    return;
+                }
+
+                float amountToLift = distanceToFloatingPoint * stateMachine.Player.ResizableCapsuleCollider.SlopeData.StepReachForce - GetPlayerVerticalVelocity().y;
+
+                Vector3 liftForce = new Vector3(0f, amountToLift, 0f);
+
+                stateMachine.Player.Rigidbody.AddForce(liftForce, ForceMode.VelocityChange);
+            }
         }
 
         protected override void AddInputActionsCallbacks()
