084c4da2f1e418008d7970bebadf0333bbdc896f
Wafflus <wafflusdev@gmail.com>
2022-04-11 19:25:41 +0100

    Add slope speed angles animation curve property to the player grounded data script

--- Assets/Scripts/Characters/Player/Data/States/Grounded/PlayerGroundedData.cs
+++ Assets/Scripts/Characters/Player/Data/States/Grounded/PlayerGroundedData.cs
@@ -7,6 +7,7 @@ namespace GenshinImpactMovementSystem
     public class PlayerGroundedData
     {
         [field: SerializeField] [field: Range(0f, 25f)] public float BaseSpeed { get; private set; } = 5f;
+        [field: SerializeField] public AnimationCurve SlopeSpeedAngles { get; private set; }
         [field: SerializeField] public PlayerRotationData BaseRotationData { get; private set; }
         [field: SerializeField] public PlayerWalkData WalkData { get; private set; }
         [field: SerializeField] public PlayerRunData RunData { get; private set; }
