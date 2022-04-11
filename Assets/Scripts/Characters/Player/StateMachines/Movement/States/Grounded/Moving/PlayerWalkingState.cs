84517935b170a0db3d4685054794e448d3ddbac1
Wafflus <wafflusdev@gmail.com>
2022-04-11 17:03:47 +0100

    Add walking state logic

--- Assets/Scripts/Characters/Player/StateMachines/Movement/States/Grounded/Moving/PlayerWalkingState.cs
+++ Assets/Scripts/Characters/Player/StateMachines/Movement/States/Grounded/Moving/PlayerWalkingState.cs
@@ -5,5 +5,12 @@ namespace GenshinImpactMovementSystem
         public PlayerWalkingState(PlayerMovementStateMachine playerMovementStateMachine) : base(playerMovementStateMachine)
         {
         }
+
+        public override void Enter()
+        {
+            base.Enter();
+
+            speedModifier = 0.225f;
+        }
     }
 }
