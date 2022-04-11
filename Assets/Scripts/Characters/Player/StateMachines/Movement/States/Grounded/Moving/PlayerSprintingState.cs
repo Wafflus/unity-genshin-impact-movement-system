1fde658391a73b520b6f31b86e7078d7d53f9fef
Wafflus <wafflusdev@gmail.com>
2022-04-11 21:44:19 +0100

    Add state transition logic to the player sprinting state script

--- Assets/Scripts/Characters/Player/StateMachines/Movement/States/Grounded/Moving/PlayerSprintingState.cs
+++ Assets/Scripts/Characters/Player/StateMachines/Movement/States/Grounded/Moving/PlayerSprintingState.cs
@@ -1,7 +1,14 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
 namespace GenshinImpactMovementSystem
 {
     public class PlayerSprintingState : PlayerMovingState
     {
+        private float startTime;
+
+        private bool keepSprinting;
+
         public PlayerSprintingState(PlayerMovementStateMachine playerMovementStateMachine) : base(playerMovementStateMachine)
         {
         }
@@ -11,6 +18,63 @@ namespace GenshinImpactMovementSystem
             base.Enter();
 
             stateMachine.ReusableData.MovementSpeedModifier = groundedData.SprintData.SpeedModifier;
+
+            startTime = Time.time;
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+
+            keepSprinting = false;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            if (keepSprinting)
+            {
+                return;
+            }
+
+            if (Time.time < startTime + groundedData.SprintData.SprintToRunTime)
+            {
+                return;
+            }
+
+            StopSprinting();
+        }
+
+        private void StopSprinting()
+        {
+            if (stateMachine.ReusableData.MovementInput == Vector2.zero)
+            {
+                stateMachine.ChangeState(stateMachine.IdlingState);
+
+                return;
+            }
+
+            stateMachine.ChangeState(stateMachine.RunningState);
+        }
+
+        protected override void AddInputActionsCallbacks()
+        {
+            base.AddInputActionsCallbacks();
+
+            stateMachine.Player.Input.PlayerActions.Sprint.performed += OnSprintPerformed;
+        }
+
+        protected override void RemoveInputActionsCallbacks()
+        {
+            base.RemoveInputActionsCallbacks();
+
+            stateMachine.Player.Input.PlayerActions.Sprint.performed -= OnSprintPerformed;
+        }
+
+        private void OnSprintPerformed(InputAction.CallbackContext context)
+        {
+            keepSprinting = true;
         }
     }
 }
