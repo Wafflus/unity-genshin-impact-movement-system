cc51e24f5860d9ebf98317b6351cdd5a5ce71f50
Wafflus <wafflusdev@gmail.com>
2022-04-11 19:01:23 +0100

    Add float ray distance property to the slope data script

--- Assets/Scripts/Data/Colliders/SlopeData.cs
+++ Assets/Scripts/Data/Colliders/SlopeData.cs
@@ -7,5 +7,6 @@ namespace GenshinImpactMovementSystem
     public class SlopeData
     {
         [field: SerializeField] [field: Range(0f, 1f)] public float StepHeightPercentage { get; private set; } = 0.25f;
+        [field: SerializeField] [field: Range(0f, 5f)] public float FloatRayDistance { get; private set; } = 2f;
     }
 }
