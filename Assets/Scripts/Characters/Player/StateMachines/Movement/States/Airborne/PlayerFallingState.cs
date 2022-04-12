4db3d1343520fc003a408100fd4a8573b3c98d8e
Wafflus <wafflusdev@gmail.com>
2022-04-12 13:42:38 +0100

    Add player falling state script

---
+++ Assets/Scripts/Characters/Player/StateMachines/Movement/States/Airborne/PlayerFallingState.cs
@@ -0,0 +1,9 @@
+namespace GenshinImpactMovementSystem
+{
+    public class PlayerFallingState : PlayerAirborneState
+    {
+        public PlayerFallingState(PlayerMovementStateMachine playerMovementStateMachine) : base(playerMovementStateMachine)
+        {
+        }
+    }
+}
