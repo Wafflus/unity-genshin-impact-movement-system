c61338f0c368c4d7cf8aa5629c4e27534fb28228
Wafflus <wafflusdev@gmail.com>
2022-04-12 11:30:05 +0100

    Add contains layer method to the player data script

--- Assets/Scripts/Characters/Player/Data/Layers/PlayerLayerData.cs
+++ Assets/Scripts/Characters/Player/Data/Layers/PlayerLayerData.cs
@@ -7,5 +7,10 @@ namespace GenshinImpactMovementSystem
     public class PlayerLayerData
     {
         [field: SerializeField] public LayerMask GroundLayer { get; private set; }
+
+        public bool ContainsLayer(LayerMask layerMask, int layer)
+        {
+            return (1 << layer & layerMask) != 0;
+        }
     }
 }
