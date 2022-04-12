b3dd6ece6d4d5b3ee3d9a82aa09fe90c38eef822
Wafflus <wafflusdev@gmail.com>
2022-04-12 11:39:21 +0100

    Add rotation data property to the player jump data script

--- Assets/Scripts/Characters/Player/Data/States/Airborne/PlayerJumpData.cs
+++ Assets/Scripts/Characters/Player/Data/States/Airborne/PlayerJumpData.cs
@@ -6,6 +6,7 @@ namespace GenshinImpactMovementSystem
     [Serializable]
     public class PlayerJumpData
     {
+        [field: SerializeField] public PlayerRotationData RotationData { get; private set; }
         [field: SerializeField] public Vector3 StationaryForce { get; private set; }
         [field: SerializeField] public Vector3 WeakForce { get; private set; }
         [field: SerializeField] public Vector3 MediumForce { get; private set; }
