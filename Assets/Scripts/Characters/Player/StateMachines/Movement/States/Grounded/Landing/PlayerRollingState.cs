ef836126925d7a7d68f1ec8240c6ec7ba83f716b
Wafflus <wafflusdev@gmail.com>
2022-04-12 15:18:14 +0100

    Add rolling state logic

--- Assets/Scripts/Characters/Player/StateMachines/Movement/States/Grounded/Landing/PlayerRollingState.cs
+++ Assets/Scripts/Characters/Player/StateMachines/Movement/States/Grounded/Landing/PlayerRollingState.cs
@@ -1,9 +1,32 @@
+using UnityEngine;
+
 namespace GenshinImpactMovementSystem
 {
     public class PlayerRollingState : PlayerLandingState
     {
         public PlayerRollingState(PlayerMovementStateMachine playerMovementStateMachine) : base(playerMovementStateMachine)
         {
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+
+            stateMachine.ReusableData.MovementSpeedModifier = groundedData.RollData.SpeedModifier;
+
+            stateMachine.ReusableData.ShouldSprint = false;
+        }
+
+        public override void PhysicsUpdate()
+        {
+            base.PhysicsUpdate();
+
+            if (stateMachine.ReusableData.MovementInput != Vector2.zero)
+            {
+                return;
+            }
+
+            RotateTowardsTargetRotation();
         }
     }
 }
