ab7db09a2a350936c25ffca55ac8b961dec4b872
Wafflus <wafflusdev@gmail.com>
2022-04-12 11:10:58 +0100

    Add base jumping state logic

--- Assets/Scripts/Characters/Player/StateMachines/Movement/States/Airborne/PlayerJumpingState.cs
+++ Assets/Scripts/Characters/Player/StateMachines/Movement/States/Airborne/PlayerJumpingState.cs
@@ -1,9 +1,34 @@
+using UnityEngine;
+
 namespace GenshinImpactMovementSystem
 {
     public class PlayerJumpingState : PlayerAirborneState
     {
         public PlayerJumpingState(PlayerMovementStateMachine playerMovementStateMachine) : base(playerMovementStateMachine)
         {
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+
+            stateMachine.ReusableData.MovementSpeedModifier = 0f;
+
+            Jump();
+        }
+
+        private void Jump()
+        {
+            Vector3 jumpForce = stateMachine.ReusableData.CurrentJumpForce;
+
+            Vector3 playerForward = stateMachine.Player.transform.forward;
+
+            jumpForce.x *= playerForward.x;
+            jumpForce.z *= playerForward.z;
+
+            ResetVelocity();
+
+            stateMachine.Player.Rigidbody.AddForce(jumpForce, ForceMode.VelocityChange);
         }
     }
 }
