c05d1bd0c47f3ad91bff31a62a98f4676e39416e
Wafflus <wafflusdev@gmail.com>
2022-04-12 10:34:15 +0100

    Add automatic rotation to the player stopping state script

--- Assets/Scripts/Characters/Player/StateMachines/Movement/States/Grounded/Stopping/PlayerStoppingState.cs
+++ Assets/Scripts/Characters/Player/StateMachines/Movement/States/Grounded/Stopping/PlayerStoppingState.cs
@@ -19,6 +19,8 @@ namespace GenshinImpactMovementSystem
         {
             base.PhysicsUpdate();
 
+            RotateTowardsTargetRotation();
+
             if (!IsMovingHorizontally())
             {
                 return;
