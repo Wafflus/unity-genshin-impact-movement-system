4b1d7411daf120c3b807e4f353a1658b84ca2018
Wafflus <wafflusdev@gmail.com>
2022-04-12 11:04:46 +0100

    Add player airborne data reference to the player scriptable object script

--- Assets/Scripts/Characters/Player/Data/ScriptableObjects/PlayerSO.cs
+++ Assets/Scripts/Characters/Player/Data/ScriptableObjects/PlayerSO.cs
@@ -6,5 +6,6 @@ namespace GenshinImpactMovementSystem
     public class PlayerSO : ScriptableObject
     {
         [field: SerializeField] public PlayerGroundedData GroundedData { get; private set; }
+        [field: SerializeField] public PlayerAirborneData AirborneData { get; private set; }
     }
 }
