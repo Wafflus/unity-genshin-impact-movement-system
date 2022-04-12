6b298bbf4ebbf31fd50cf879f3b639b334ecdc73
Wafflus <wafflusdev@gmail.com>
2022-04-12 14:55:23 +0100

    Add light landing state logic

--- Assets/Scripts/Characters/Player/StateMachines/Movement/States/Grounded/Landing/PlayerLightLandingState.cs
+++ Assets/Scripts/Characters/Player/StateMachines/Movement/States/Grounded/Landing/PlayerLightLandingState.cs
@@ -5,5 +5,16 @@ namespace GenshinImpactMovementSystem
         public PlayerLightLandingState(PlayerMovementStateMachine playerMovementStateMachine) : base(playerMovementStateMachine)
         {
         }
+
+        public override void Enter()
+        {
+            base.Enter();
+
+            stateMachine.ReusableData.MovementSpeedModifier = 0;
+
+            stateMachine.ReusableData.CurrentJumpForce = airborneData.JumpData.StationaryForce;
+
+            ResetVelocity();
+        }
     }
 }
