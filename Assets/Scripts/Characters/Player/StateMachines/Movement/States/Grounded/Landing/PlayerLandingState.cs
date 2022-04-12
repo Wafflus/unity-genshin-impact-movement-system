c376bdfa5d226b2f57f6de60e7a0b473c05acce5
Wafflus <wafflusdev@gmail.com>
2022-04-12 15:18:40 +0100

    Add state transition logic to the player landing state script

--- Assets/Scripts/Characters/Player/StateMachines/Movement/States/Grounded/Landing/PlayerLandingState.cs
+++ Assets/Scripts/Characters/Player/StateMachines/Movement/States/Grounded/Landing/PlayerLandingState.cs
@@ -1,8 +1,14 @@
+using UnityEngine.InputSystem;
+
 namespace GenshinImpactMovementSystem
 {
     public class PlayerLandingState : PlayerGroundedState
     {
         public PlayerLandingState(PlayerMovementStateMachine playerMovementStateMachine) : base(playerMovementStateMachine)
+        {
+        }
+
+        protected override void OnMovementCanceled(InputAction.CallbackContext context)
         {
         }
     }
