c59f66d34642dc22fb713048d413711a50811240
Wafflus <wafflusdev@gmail.com>
2022-04-12 12:31:47 +0100

    Add on trigger exit method to the state interface script

--- Assets/Scripts/StateMachine/IState.cs
+++ Assets/Scripts/StateMachine/IState.cs
@@ -10,6 +10,7 @@ namespace GenshinImpactMovementSystem
         public void Update();
         public void PhysicsUpdate();
         public void OnTriggerEnter(Collider collider);
+        public void OnTriggerExit(Collider collider);
         public void OnAnimationEnterEvent();
         public void OnAnimationExitEvent();
         public void OnAnimationTransitionEvent();
