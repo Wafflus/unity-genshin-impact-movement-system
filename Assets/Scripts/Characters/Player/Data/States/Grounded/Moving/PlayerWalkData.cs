19e75957f91e1af520115ace3e631ab5a1a955c8
Wafflus <wafflusdev@gmail.com>
2022-04-12 16:54:35 +0100

    Add backwards recentering data list property to the player walk data script

--- Assets/Scripts/Characters/Player/Data/States/Grounded/Moving/PlayerWalkData.cs
+++ Assets/Scripts/Characters/Player/Data/States/Grounded/Moving/PlayerWalkData.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace GenshinImpactMovementSystem
@@ -7,5 +8,6 @@ namespace GenshinImpactMovementSystem
     public class PlayerWalkData
     {
         [field: SerializeField] [field: Range(0f, 1f)] public float SpeedModifier { get; private set; } = 0.225f;
+        [field: SerializeField] public List<PlayerCameraRecenteringData> BackwardsCameraRecenteringData { get; private set; }
     }
 }
