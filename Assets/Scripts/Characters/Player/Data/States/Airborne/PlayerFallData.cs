d96114d7fe4dd1ecb277de865389220094eb57e8
Wafflus <wafflusdev@gmail.com>
2022-04-12 15:27:30 +0100

    Add minimum distance to be considered hard fall property to the player fall data script

--- Assets/Scripts/Characters/Player/Data/States/Airborne/PlayerFallData.cs
+++ Assets/Scripts/Characters/Player/Data/States/Airborne/PlayerFallData.cs
@@ -7,5 +7,6 @@ namespace GenshinImpactMovementSystem
     public class PlayerFallData
     {
         [field: SerializeField] [field: Range(1f, 15f)] public float FallSpeedLimit { get; private set; } = 15f;
+        [field: SerializeField] [field: Range(0f, 100f)] public float MinimumDistanceToBeConsideredHardFall { get; private set; } = 3f;
     }
 }
