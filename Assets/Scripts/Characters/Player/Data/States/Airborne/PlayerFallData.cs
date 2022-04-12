29fc31e5e9266e245afe8ea8bd52ddce1eca6626
Wafflus <wafflusdev@gmail.com>
2022-04-12 20:04:36 +0100

    Update fall velocity limit to a lower value

--- Assets/Scripts/Characters/Player/Data/States/Airborne/PlayerFallData.cs
+++ Assets/Scripts/Characters/Player/Data/States/Airborne/PlayerFallData.cs
@@ -6,7 +6,8 @@ namespace GenshinImpactMovementSystem
     [Serializable]
     public class PlayerFallData
     {
-        [field: SerializeField] [field: Range(1f, 15f)] public float FallSpeedLimit { get; private set; } = 15f;
+        [field: Tooltip("Having higher numbers might not read collisions with shallow colliders correctly.")]
+        [field: SerializeField] [field: Range(0f, 10f)] public float FallSpeedLimit { get; private set; } = 10f;
         [field: SerializeField] [field: Range(0f, 100f)] public float MinimumDistanceToBeConsideredHardFall { get; private set; } = 3f;
     }
 }
